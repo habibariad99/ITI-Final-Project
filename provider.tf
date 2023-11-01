427d2b4e37281c25a3b77f3c74ceca8de4c088bc
habibariad99 <habiba.riad123@gmail.com>
2023-11-01 03:04:58 +0200

    infra using terraform

--- provider.tf
+++
@@ -1,7 +0,0 @@
-#hena b3ml setting lel provider w region 
-
-provider "google" {
-  project     =  "gcp-jenkins-terraform"
-  region      =  "us-east1"
-  credentials = file("/home/kimo/Downloads/gcp-jenkins-terraform-262e3629ccbf.json")
-}
